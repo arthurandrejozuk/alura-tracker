c28788e8709c8fbc5bf3ee8437beb94f2a55245f
arthurandrejozuk <arthurpro295@gmail.com>
2024-10-08 08:09:55 -0300

    Iniciando estudos de Vue.js

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>
+            <img src="https://raw.githubusercontent.com/alura-cursos/alura-tracker/main/src/assets/logo.png" alt="Logo Alura Tracker">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<style>
+header {
+  padding: 1rem;
+  padding-top: 2rem;
+  padding-left: 2rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
---
+++ src/components/Temporizador.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="is-flex is-align-items-center is-justify-content-space-evenly">
+        <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+        <BotaoTemporizador @clicado="iniciar" icone="fas fa-play" texto="Play" :desabilitado="cronometroRodando"/>
+        <BotaoTemporizador @clicado="finalizar" icone="fas fa-stop" texto="Stop" :desabilitado="!cronometroRodando"/>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
+import BotaoTemporizador from './BotaoTemporizador.vue';
+
+export default defineComponent({
+    name: 'Temporizador',
+    emits: ['aoTemporizadorFinalizado'],
+    components: {
+        Cronometro,
+        BotaoTemporizador
+    },
+    data() {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0,
+            cronometroRodando: false
+        }
+    },
+    methods: {
+        iniciar() {
+            this.cronometroRodando = true
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1;
+            }, 1000)
+        },
+        finalizar() {
+            this.cronometroRodando = false
+            clearInterval(this.cronometro)
+            this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos)
+            this.tempoEmSegundos = 0
+        }
+    }
+})
+</script>
